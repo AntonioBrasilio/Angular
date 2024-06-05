738c1077394521a717f20942143da824a55b2494
Antonio Brasilio <antonioalves05555@gmail.com>
2024-06-05 02:24:23 -0300

    feat: Using hightlighted class

--- angular-course-2-directives/src/app/directives/highlighted.directive.ts
+++ angular-course-2-directives/src/app/directives/highlighted.directive.ts
@@ -1,8 +1,18 @@
-import { Directive } from "@angular/core";
+import { Directive, HostBinding } from "@angular/core";
 
 @Directive({
     selector: "[highlighted]",
 })
 export class HighlightedDirective {
     constructor() {}
+
+    @HostBinding("class.highlighted")
+    get applyStyle() {
+        return true;
+    }
+
+    @HostBinding("attr.disabled")
+    get applyDisabled() {
+        return true;
+    }
 }
