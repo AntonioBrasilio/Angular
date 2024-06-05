e38a57832020fa255035987c201bf8e7a7cd14ae
Antonio Brasilio <antonioalves05555@gmail.com>
2024-06-05 02:55:10 -0300

    feat: Using HostListener

--- angular-course-2-directives/src/app/app.component.ts
+++ angular-course-2-directives/src/app/app.component.ts
@@ -1,32 +1,26 @@
-import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements AfterViewInit {
-
     courses = COURSES;
 
+    @ViewChildren(CourseCardComponent, { read: ElementRef })
+    cards: QueryList<ElementRef>;
 
-    @ViewChildren(CourseCardComponent, {read: ElementRef})
-    cards : QueryList<ElementRef>;
+    constructor() {}
 
-
-    constructor() {
-
+    onToggleHightlighted(isHighlighted: boolean) {
+        console.log("isHighlighted", isHighlighted);
     }
 
-    ngAfterViewInit() {
+    ngAfterViewInit() {}
 
-    }
-
-    onCourseSelected(course:Course) {
-
-    }
-
+    onCourseSelected(course: Course) {}
 }
--- angular-course-2-directives/src/app/directives/highlighted.directive.ts
+++ angular-course-2-directives/src/app/directives/highlighted.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding } from "@angular/core";
+import { Directive, Host, HostBinding, HostListener, Output, EventEmitter } from "@angular/core";
 
 @Directive({
     selector: "[highlighted]",
@@ -6,13 +6,31 @@ import { Directive, HostBinding } from "@angular/core";
 export class HighlightedDirective {
     constructor() {}
 
+    isHighlighted = false;
+
+    @Output()
+    highlightedEvent = new EventEmitter();
+
     @HostBinding("class.highlighted")
     get applyStyle() {
-        return true;
+        return this.isHighlighted;
     }
 
     @HostBinding("attr.disabled")
     get applyDisabled() {
         return true;
+    }
+
+    @HostListener("mouseover", ["$event"])
+    onMouseOver($event) {
+        console.log($event);
+        this.isHighlighted = true;
+        this.highlightedEvent.emit(this.isHighlighted);
+    }
+
+    @HostListener("mouseleave")
+    onMouseLeave() {
+        this.isHighlighted = false;
+        this.highlightedEvent.emit(this.isHighlighted);
     }
 }
