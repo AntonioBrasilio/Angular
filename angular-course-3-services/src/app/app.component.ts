1706e5fe61718b1b2b2e895a445c01a655b43427
Antonio Brasilio <antonioalves05555@gmail.com>
2024-06-06 04:30:16 -0300

    feat: Creating a new service

--- angular-course-3-services/src/app/app.component.ts
+++ angular-course-3-services/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Course } from "./model/course";
 import { CourseCardComponent } from "./course-card/course-card.component";
 import { HighlightedDirective } from "./directives/highlighted.directive";
 import { Observable } from "rxjs";
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { CoursesService } from "./services/courses.service";
 
 @Component({
     selector: "app-root",
@@ -14,11 +14,9 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 export class AppComponent implements OnInit {
     courses$ = new Observable<Course[]>();
 
-    constructor(private http: HttpClient) {}
+    constructor(private coursesService: CoursesService) {}
 
     ngOnInit() {
-        const params = new HttpParams().set("page", "1").set("pageSize", "10");
-
-        this.courses$ = this.http.get<Course[]>("/api/courses", { params });
+        this.courses$ = this.coursesService.loadCourses();
     }
 }
